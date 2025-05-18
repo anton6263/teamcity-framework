84c5bda53a3426e84b5a9060eb16e0e555ecd2c9
aa.arteev <aa.arteev@severmed.com>
2025-05-18 19:54:09 +0300

    update configs

--- src/test/java/com/example/teamcity/api/BuildConfigurationTest.java
+++ src/test/java/com/example/teamcity/api/BuildConfigurationTest.java
@@ -1,5 +1,7 @@
 package com.example.teamcity.api;
 
+import com.example.teamcity.api.models.User;
+import com.example.teamcity.api.spec.Specifications;
 import io.restassured.RestAssured;
 import org.apache.http.HttpStatus;
 import org.testng.annotations.Test;
@@ -8,15 +10,17 @@ public class BuildConfigurationTest extends BaseApiTest {
 
     @Test
     public void buildConfigurationTest() {
-        var token = RestAssured.given().auth()
-                .basic("admin", "admin")
-                .when()
-                .get("http://192.168.31.92:8111/authenticationTest.html?csrf")
-                .then()
-                .assertThat()
-                .statusCode(HttpStatus.SC_OK)
-                .extract()
-                .asString();
+        User user = User.builder()
+                .username("admin")
+                .password("admin")
+                .build();
+
+        var token = RestAssured
+                .given()
+                .spec(Specifications.getSpec().authSpec(user))
+                .get("/authenticationTest.html?csrf")
+                .then().assertThat().statusCode(HttpStatus.SC_OK)
+                .extract().asString();
 
         System.out.println(token);
     }
